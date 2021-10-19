6d953cab97825195d3f4d520a9b8744c8bda4c0a
Toumani <toumani49@gmail.com>
2021-10-19 13:40:20 +0100

    fix: fix favicon

--- src/main/java/com/kststudios/noties/Application.java
+++ src/main/java/com/kststudios/noties/Application.java
@@ -18,7 +18,7 @@ import com.vaadin.flow.theme.Theme;
  */
 @SpringBootApplication
 @Theme(value = "noties")
-@PWA(name = "Noties", shortName = "Noties", offlineResources = {"images/logo.png"})
+@PWA(name = "Noties", shortName = "Noties", iconPath = "images/logo.png", offlineResources = {"images/logo.png"})
 public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
 
     public static void main(String[] args) {
